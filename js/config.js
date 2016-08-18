50df7ede53de456c66540acd3699310f98d29e92
George Hamilton <gmh04@netscape.net>
2016-08-18 15:24:08 +0100

    modify config for cat survey app: https://github.com/edina/fieldtrip-cps/issues/22

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 /** Global configuration options */
 // NOTE: we can encapsulate that only *after* we start requiring invariants for these variables
 
-/* Pulbic USER 
+/* Public USER
 * @param {string} PUBLIC_UUID
 */
 PUBLIC_UUID = "2338e388-f34e-25d9-945c-54cffd9c46c2";
@@ -9,17 +9,17 @@ PUBLIC_UUID = "2338e388-f34e-25d9-945c-54cffd9c46c2";
 /* Is this a PCAPI geojson or a WFS geojson ? ( PCAPI is richer but cannot be handled by a WFS )
 * @param endpoint_type - Can be PCAPI or WFS
 */
-ENDPOINT_TYPE = "WFS";
+ENDPOINT_TYPE = "PCAPI";
 
 /* Access a private or a public endpoint ? (Many SAML endpoint differ depending on openess)
 * @param {bool} private_endpoint
 */
-PRIVATE_SURVEY = false;
+PRIVATE_SURVEY = true;
 
 /* Whether we are testing using local file urls instead of endpoint or not -- this will change all endpoints to local files
 * @param {bool} test
 */
-TEST = true;
+TEST = false;
 
 /* default values for endpoints -- Will return the right endpoint depednign on type (eg. WFS), test-ing or privacy settings
 * @param {bool} private_endpoints
@@ -27,10 +27,10 @@ TEST = true;
 ENDPOINTS = {
     "PCAPI" : {
                 test_url: "data/Snowdonia National Park Japanese Knotweed Survey - poly.geojson?",
-                private_base_url: 'https://dyfi.cobwebproject.eu/1.3/pcapi/records/local/00000000-0000-0000-0000-000000000000/?',
-                public_base_url: 'https://dyfi.cobwebproject.eu/1.3/pcapi/records/local/' + PUBLIC_UUID + '/?',
-                private_image_base: "https://dyfi.cobwebproject.eu/1.3/pcapi/records/local/00000000-0000-0000-0000-000000000000/",
-                public_image_base: 'https://dyfi.cobwebproject.eu/1.3/pcapi/records/local/' + PUBLIC_UUID + '/'
+                private_base_url: '/1.3/pcapi/records/local/00000000-0000-0000-0000-000000000000/?',
+                public_base_url: '/1.3/pcapi/records/local/' + PUBLIC_UUID + '/?',
+                private_image_base: "/1.3/pcapi/records/local/00000000-0000-0000-0000-000000000000/",
+                public_image_base: '/1.3/pcapi/records/local/' + PUBLIC_UUID + '/'
             },
     "WFS" : {
                 // constant url pointing to local file for unit testing
